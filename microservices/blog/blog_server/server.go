d3f64c4a15f83b51155663193b8f10c34860b8f3
kiquetal <kiquetal@gmail.com>
2024-02-11 13:47:31 -0300

    adding mongo driver

--- microservices/blog/blog_server/server.go
+++ microservices/blog/blog_server/server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"grpc/learning/blog/blogpb"
 	"log"
@@ -14,9 +17,33 @@ type server struct {
 	blogpb.UnimplementedBlogServiceServer
 }
 
+var collection *mongo.Collection
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	fmt.Printf("Blog Service Started")
+	fmt.Printf("Blog Service Started\n")
+
+	var options = options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(options.Credential{
+		Username:      "mydbadmin",
+		Password:      "admin",
+		AuthSource:    "mydb",
+		AuthMechanism: "SCRAM-SHA-256",
+	})
+	client, err := mongo.Connect(context.Background(), options)
+	if err != nil {
+		log.Fatalf("Error creating mongo client: %v", err)
+	}
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error pinging mongo client: %v", err)
+	}
+	fmt.Println("Connected to MongoDB")
+
+	collection = client.Database("mydb").Collection("blog")
+
+	if collection == nil {
+		log.Fatalf("Error getting collection: %v", err)
+	}
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
--- microservices/notes.md
+++ microservices/notes.md
@@ -9,3 +9,9 @@ go modules -> enable go module integration
 ### Start mongoo
 docker run -d  --name my-mongo -p 27017:27017   -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=admin mongo  
 
+mydb
+user: mydbadmin
+pass: admin
+
+mongosh -u mydbadmin -p admin --authenticationDatabase mydb
+
